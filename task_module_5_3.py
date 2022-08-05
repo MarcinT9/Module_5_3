bbd6f19ddcf1268aff2acde60cac621d92d40336
Marcin Topczewski <marcintopczewski90@gmail.com>
2022-08-05 16:38:39 +0200

    Correct file

--- task_module_5_3.py
+++ task_module_5_3.py
@@ -9,6 +9,8 @@ class BaseContact:
         self.phone_number = phone_number
         self.email = email
 
+        self._len_adressee = 0
+
     def __str__(self):
         return f'{self.first_name} {self.last_name} {self.phone_number} {self.email}'
 
@@ -20,8 +22,11 @@ class BaseContact:
 
     @property
     def len_adressee(self):
-        self._len_adressee = len(self.first_name) + len(self.last_name) + 1
         return self._len_adressee
+
+    @len_adressee.setter
+    def len_adressee(self, value):
+        self._len_adressee = len(self.first_name) + len(self.last_name) + 1
 
 class BusinessContact(BaseContact):
     def __init__(self, position, company, business_phone, *args, **kwargs):
@@ -43,11 +48,15 @@ def create_contacts(type, value):
     for i in range(choice2):
         if choice == 1:
             fake_card = BaseContact(first_name=fake.first_name(), last_name=fake.last_name(), phone_number=fake.phone_number(), email=fake.email())
+            print(fake_card)
             print(fake_card.contact())
+            fake_card.len_adressee = 0
             print(f'Długość imienia i nazwiska wynosi {fake_card.len_adressee} znaków.')
         elif choice == 2:
             fake_card2 = BusinessContact(first_name=fake.first_name(), last_name=fake.last_name(), phone_number=fake.phone_number(), email=fake.email(), position=fake.job(), company=fake.company(), business_phone=fake.phone_number())
+            print(fake_card2)
             print(fake_card2.contact())
+            fake_card2.len_adressee = 0
             print(f'Długość imienia i nazwiska wynosi {fake_card2.len_adressee} znaków.')
 
 if __name__ == '__main__':
